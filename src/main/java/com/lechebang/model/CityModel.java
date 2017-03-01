4d5f142eb309265dc0c3d01f2274b11601be6a25
javarcn <javar.cn@gmail.com>
2017-03-01 23:55:12 +0800

    json

---
+++ src/main/java/com/lechebang/model/CityModel.java
@@ -0,0 +1,61 @@
+package com.lechebang.model;
+
+/**
+ * Created by Administrator on 2017/3/1.
+ */
+public class CityModel {
+    private int costTime;
+    private String msg;
+    private CityResult result=new CityResult();
+    private String resultCode;
+    private String statusCode;
+    private Object validationErrors;
+
+    public int getCostTime() {
+        return costTime;
+    }
+
+    public void setCostTime(int costTime) {
+        this.costTime = costTime;
+    }
+
+    public String getMsg() {
+        return msg;
+    }
+
+    public void setMsg(String msg) {
+        this.msg = msg;
+    }
+
+    public CityResult getResult() {
+        return result;
+    }
+
+    public void setResult(CityResult result) {
+        this.result = result;
+    }
+
+    public String getResultCode() {
+        return resultCode;
+    }
+
+    public void setResultCode(String resultCode) {
+        this.resultCode = resultCode;
+    }
+
+    public String getStatusCode() {
+        return statusCode;
+    }
+
+    public void setStatusCode(String statusCode) {
+        this.statusCode = statusCode;
+    }
+
+    public Object getValidationErrors() {
+        return validationErrors;
+    }
+
+    public void setValidationErrors(Object validationErrors) {
+        this.validationErrors = validationErrors;
+    }
+}
