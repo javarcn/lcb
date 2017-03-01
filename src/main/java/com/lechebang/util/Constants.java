5117458bd99b08d307d680dc3fb865a74c7c9ac5
jiacheng <543859820@qq.com>
2017-03-01 12:28:28 +0800

    Constants

---
+++ src/main/java/com/lechebang/util/Constants.java
@@ -0,0 +1,11 @@
+package com.lechebang.util;
+
+/**
+ * Author: jiacheng
+ * Date: 2017/3/1
+ */
+public class Constants {
+    public final static String  TOKEN="9edff60b6dec104bcb6e25549710834e";
+    public final static int APPCODE=101;
+    public final static String LCB_CLIENT_ID ="cd3585e5-d591-4f07-a531-e825cec9e82c";
+}
