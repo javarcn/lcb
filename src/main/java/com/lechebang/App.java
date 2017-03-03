64ccbc8994c90983124a78f960a555e4511b61df
javarcn <javar.cn@gmail.com>
2017-03-04 03:10:37 +0800

    json

--- src/main/java/com/lechebang/App.java
+++ src/main/java/com/lechebang/App.java
@@ -16,12 +16,28 @@ public class App {
 
     public static void main( String[] args )
     {
-        logger.info( "数据爬取开始！" );
+        System.out.println( "数据爬取开始！" );
         //TODO 获取城市
         logger.info("获取城市****************************************");
         List<City> cityList=GetCitys.list();
         for (City city:cityList){
-            logger.info(city.toString());
+            System.out.println(city.toString());
+            //TODO 获取品牌
+            List<Brand> brandList=GetCarBrands.list(city.getCode());
+            for(Brand brand:brandList){
+                //TODO 获取型号
+                List<CarTypeResult> carTypeResultList =GetCarType.list(brand.getId(),city.getCode());
+                for(CarTypeResult result: carTypeResultList){
+                    for(CarType type:result.getBrandProduceCar()){
+                        //TODO 获取车款配置
+//                        System.out.println(result.getProducerName()+type.toString());
+                        List<TypeDetail> typeDetailList=GetCarTypeDetail.list(type.getId());
+                        for(TypeDetail typeDetail:typeDetailList){
+                            System.out.println(typeDetail.toString());
+                        }
+                    }
+                }
+            }
         }
         //TODO 获取品牌
         logger.info("获取品牌****************************************");
@@ -72,6 +88,7 @@ public class App {
         //TODO 删除汽车
         logger.info("删除汽车****************************************");
         logger.info(DelMyCar.ok(1815965));
+        System.out.println( "数据爬取结束！" );
 
     }
 }
